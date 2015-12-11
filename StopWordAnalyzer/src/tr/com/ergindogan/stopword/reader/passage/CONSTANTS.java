ca5837137f6ebd6e125c113eb9febc4aa48a9eb8
Ergin Dogan Yildiz <ergindogan@gmail.com>
2015-12-11 02:20:00 +0200

    CONSTANS added. Distinc authors retreived to classify passages according to authors first.

---
+++ StopWordAnalyzer/src/tr/com/ergindogan/stopword/reader/passage/CONSTANTS.java
@@ -0,0 +1,11 @@
+package tr.com.ergindogan.stopword.reader.passage;
+
+/**
+ * @author ergindoganyildiz
+ * 
+ * Dec 11, 2015
+ */
+public final class CONSTANTS {
+
+	public static final String SEPERATOR = "------------------------------------------------------------------------------------------";
+}
