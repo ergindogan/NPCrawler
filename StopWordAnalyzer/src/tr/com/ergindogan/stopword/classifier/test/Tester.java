0060c6c002b800b9658f4dd23be1e620376e0217
Ergin Dogan Yildiz <ergindogan@gmail.com>
2016-01-13 15:58:56 +0200

    Redundant functions moved the super class.

--- StopWordAnalyzer/src/tr/com/ergindogan/stopword/classifier/test/Tester.java
+++ StopWordAnalyzer/src/tr/com/ergindogan/stopword/classifier/test/Tester.java
@@ -12,12 +12,7 @@ import tr.com.ergindogan.stopword.classifier.train.Trainer;
  * 
  * Dec 11, 2015
  */
-public class Tester {
-	
-	private Map<String,List<FeatureVector>> testMap;
-	private Trainer trainer;
-	
-	public static Map<String,Map<String,Integer>> choices = new HashMap<String, Map<String,Integer>>();
+public class Tester extends BaseTester<FeatureVector>{
 	
 	public Tester(Map<String,List<FeatureVector>> testMap, Trainer trainer){
 		setTestMap(testMap);
@@ -37,7 +32,7 @@ public class Tester {
 			for(FeatureVector vector : authorsVectorList){
 				probabilityMap = new HashMap<String,Double>();
 				//test edicegimiz yazi = vector. Bu yaziyi icin butun olasi yazarlara ait olup olmadıgina bakmaliyiz.
-				for(String possibleAuthorName:getTrainer().getAuthorFeatureVectorMap().keySet()){
+				for(String possibleAuthorName: ((Trainer) getTrainer()).getAuthorFeatureVectorMap().keySet()){
 					//vector un possibleAuthorName yazar a ait olma olasiligini hesaplayacagiz. 
 					//Bu olasiliklari probabilityMap icine koyacaz.
 					double probability = calculateProbability(vector, possibleAuthorName);
@@ -47,18 +42,18 @@ public class Tester {
 				//Map icindeki en buyuk olasiliga sahip keyi testAuthorName ile karsilastiricaz.
 				String guessedAuthorName = getAuthorNameOfHighestProbability(probabilityMap);
 				
-				if(choices.get(testAuthorName) == null){
+				if(getChoices().get(testAuthorName) == null){
 					guesses = new HashMap<String, Integer>();
 					guesses.put(guessedAuthorName, 1);
 				}else{
-					guesses = choices.get(testAuthorName);
+					guesses = getChoices().get(testAuthorName);
 					if(guesses.get(guessedAuthorName) == null){
 						guesses.put(guessedAuthorName, 1);
 					}else{
 						guesses.put(guessedAuthorName, guesses.get(guessedAuthorName) + 1);
 					}
 				}
-				choices.put(testAuthorName, guesses);
+				getChoices().put(testAuthorName, guesses);
 				
 				if(testAuthorName.equals(guessedAuthorName)){
 					testResult.increaseCorrectGuessCounter(1);
@@ -71,22 +66,8 @@ public class Tester {
 		return testResult;
 	}
 	
-	private String getAuthorNameOfHighestProbability(Map<String,Double> probabilityMap){
-		String highestProbabilityAuthorName = "";
-		double highestProbability = -1;
-		
-		for(String authorName : probabilityMap.keySet()){
-			double probability = probabilityMap.get(authorName);
-			if(probability > highestProbability){
-				highestProbability = probability;
-				highestProbabilityAuthorName = authorName;
-			}
-		}
-		
-		return highestProbabilityAuthorName;
-	}
-	
-	private double calculateProbability(FeatureVector vector, String possibleAuthorName){
+	@Override
+	public double calculateProbability(FeatureVector vector, String possibleAuthorName){
 		double result = 1;
 		
 		for(int i = 0; i < vector.getVector().size(); i++){
@@ -94,33 +75,17 @@ public class Tester {
 			result = result * calculateNaiveBaesianProbability(featureValue, i, possibleAuthorName);
 		}
 		
-		return result * getTrainer().probabilityOfAuthor(possibleAuthorName);
+		return result * ((Trainer) getTrainer()).probabilityOfAuthor(possibleAuthorName);
 	}
 	
 	private double calculateNaiveBaesianProbability(double featureValue ,int featureIndex, String possibleAuthorName){
-		double standartDeviation = getTrainer().getStandartDeviation(possibleAuthorName, featureIndex);
-		double mean = getTrainer().getMean(possibleAuthorName, featureIndex);
+		double standartDeviation = ((Trainer) getTrainer()).getStandartDeviation(possibleAuthorName, featureIndex);
+		double mean = ((Trainer) getTrainer()).getMean(possibleAuthorName, featureIndex);
 		
 		double firstPart = 1 / (Math.sqrt(2 * Math.PI * Math.pow(standartDeviation, 2)));
 		double secondPart = Math.pow(Math.E, (-1 * Math.pow(featureValue - mean, 2)) / (2 * Math.pow(standartDeviation, 2)) );
 		
 		return firstPart * secondPart;
-	}
-
-	private Map<String, List<FeatureVector>> getTestMap() {
-		return testMap;
-	}
-
-	private void setTestMap(Map<String, List<FeatureVector>> testMap) {
-		this.testMap = testMap;
-	}
-
-	private Trainer getTrainer() {
-		return trainer;
-	}
-
-	private void setTrainer(Trainer trainer) {
-		this.trainer = trainer;
 	}
 
 }
