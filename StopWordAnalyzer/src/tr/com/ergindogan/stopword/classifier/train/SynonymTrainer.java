2bf5728a19e5edb8fc7640e05c20b4d57c8bfedd
Ergin Dogan Yildiz <ergindogan@gmail.com>
2016-03-11 23:19:11 +0200

    SynonymTrainer added.

---
+++ StopWordAnalyzer/src/tr/com/ergindogan/stopword/classifier/train/SynonymTrainer.java
@@ -0,0 +1,63 @@
+package tr.com.ergindogan.stopword.classifier.train;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import tr.com.ergindogan.stopword.classifier.vector.SynonymVector;
+
+public class SynonymTrainer extends BaseTrainer {
+	
+	private LinkedHashMap<String, String> mySynonymMap;
+	private Map<String, List<SynonymVector>> synonymVectorsToClassify;
+	
+	private Map<String, SynonymVector> trainMatrix;
+	
+	public SynonymTrainer(Map<String,List<SynonymVector>> synonymVectorsToClassify, LinkedHashMap<String, String> mySynonymMap){
+		setSynonymVectorsToClassify(synonymVectorsToClassify);
+		setMySynonymMap(mySynonymMap);
+		setTrainMatrix(new HashMap<String, SynonymVector>());
+	}
+
+	@Override
+	public void train() {
+		SynonymVector tempVector;
+		for(String authorName : getSynonymVectorsToClassify().keySet()){
+			List<SynonymVector> vectorList = getSynonymVectorsToClassify().get(authorName);
+			tempVector = new SynonymVector(getMySynonymMap());
+			for(SynonymVector synonymVector:vectorList){
+				//authorName yazarinin, vector yazisini train ederken
+				tempVector.addToVector(calculateConProb(synonymVector.getVector()));
+			}
+			
+			getTrainMatrix().put(authorName, tempVector);
+		}
+	}
+
+	public LinkedHashMap<String, String> getMySynonymMap() {
+		return mySynonymMap;
+	}
+
+	public void setMySynonymMap(LinkedHashMap<String, String> mySynonymMap) {
+		this.mySynonymMap = mySynonymMap;
+	}
+
+	public Map<String, List<SynonymVector>> getSynonymVectorsToClassify() {
+		return synonymVectorsToClassify;
+	}
+
+	public void setSynonymVectorsToClassify(
+			Map<String, List<SynonymVector>> synonymVectorsToClassify) {
+		this.synonymVectorsToClassify = synonymVectorsToClassify;
+	}
+
+	public Map<String, SynonymVector> getTrainMatrix() {
+		return trainMatrix;
+	}
+
+	public void setTrainMatrix(Map<String, SynonymVector> trainMatrix) {
+		this.trainMatrix = trainMatrix;
+	}
+
+}
